cf52f9041ef3d1bbb3c673d3a981c1e1050b82e4
c.x. la fontaine <cx.la.fontaine@pzh.nl>
2022-08-11 11:29:05 +0200

    aanpassing app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    # voeg artikel toe aan string die wordt getoond
     rv = "Hello, World!" + \
          "\nSee\nhttps://chrisnoring.gitbooks.io/road-to-azure/content/deployment/python-web-app-deploy.html\n"
     return rv
