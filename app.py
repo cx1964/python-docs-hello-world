e3e250c304e5286dcb706c3ec64904fa2ab77d4f
c.x. la fontaine <cx.la.fontaine@pzh.nl>
2022-08-11 11:31:15 +0200

    add article in string

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    # voeg artikel toe aan string die wordt getoond
+    # Add article to string which will be displayed
     rv = "Hello, World!" + \
          "\nSee\nhttps://chrisnoring.gitbooks.io/road-to-azure/content/deployment/python-web-app-deploy.html\n"
     return rv
